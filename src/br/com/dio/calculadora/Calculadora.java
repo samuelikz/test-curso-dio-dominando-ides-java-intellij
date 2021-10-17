1de238a9fc98f1c693532acca9e2253e8b62ae08
samuelikz <samuel.nds@outlook.com>
2021-10-17 10:01:06 -0300

    Refatorando o Codigo

--- src/br/com/dio/calculadora/Calculadora.java
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -6,34 +6,37 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int a,b;
+        int a, b;
 
         System.out.println("Digite o primeiro valor: ");
-        a=scan.nextInt();
+        a = scan.nextInt();
         System.out.println("Digite o segundo valor: ");
-        b=scan.nextInt();
+        b = scan.nextInt();
 
         int somar = somar(a, b);
         int subtrair = subtrair(a, b);
         int multiplicar = multiplicar(a, b);
         double dividir = dividir(a, b);
 
-        System.out.println("Somar: "+somar);
-        System.out.println("Subtrair: "+subtrair);
-        System.out.println("Multiplicar: "+multiplicar);
-        System.out.println("Dividir: "+dividir);
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("Dividir: " + dividir);
     }
 
-    public static int somar(int a, int b){
+    public static int somar(int a, int b) {
         return a + b;
     }
-    public static int subtrair(int a, int b){
+
+    public static int subtrair(int a, int b) {
         return a - b;
     }
-    public static int multiplicar(int a, int b){
+
+    public static int multiplicar(int a, int b) {
         return a * b;
     }
-    public static double dividir(double a, double b){
+
+    public static double dividir(double a, double b) {
         return a / b;
     }
 }
